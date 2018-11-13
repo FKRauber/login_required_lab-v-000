770a7097ea6ff6bcb8da54b3c9e449dfd1362ad5
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-13 03:52:22 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,9 @@ class SessionsController < ApplicationController
   def create
     if !params[:name] || params[:name].empty?
       redirect_to controller: 'sessions', action: 'new'
+    else
+      session[:name] = params[:name]
+      redirect_to controller: 'application', action: 'intro'
     end
   end
 
